71549855af948294e37f1a52ed62a19d34b5bf00
afekTheMiniLearner <afeksa2000@gmail.com>
2022-03-01 14:20:59 +0200

    ssaa

--- afekSfolder/smallestMissingNum.js
+++ afekSfolder/smallestMissingNum.js
@@ -1,4 +1,23 @@
-module.exports.smallestMissingNum = function(arr, result){
+/**
+ * This function gets an array of numbers and returns
+ * the smallest positive missing number in the array
+ * 
+ * @param {array} array Of numbers that the function gets 
+ * @returns {number} missingNum which contain the positive missing number in the array
+ */
 
+module.exports.smallestMissingNum = function(arr){
+  //You are given an array arr[] of N integers including 0.
+  // The task is to find the smallest positive number missing from the array.
   
+  let missingNum;
+
+  for(number of arr){
+    let currentNumber = number;
+
+    if(currentNumber > 0){
+      
+    }
+  }
 }
+console.log(this.smallestMissingNum([-8,-10,-5]));
--- afekSfolder/smallestMissingNum.test.js
+++ afekSfolder/smallestMissingNum.test.js
@@ -3,7 +3,7 @@ const {smallestMissingNum} = ('./require');
 test.each([
   [[1,2,3,4,5],6]
   [[0,-10,1,3,-20],2]
-])('smallestMissingNum(arr) return 6',
+])('smallestMissingNum return expected num',
     (arr, expected) => {
   expect(smallestMissingNum(arr)).toBe(expected);
 })
