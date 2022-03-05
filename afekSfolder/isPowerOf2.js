0df026415e416f5a0846d59a2b5a447e0ba6dc8c
afekTheMiniLearner <afeksa2000@gmail.com>
2022-03-05 15:17:14 +0200

    333

---
+++ afekSfolder/isPowerOf2.js
@@ -0,0 +1,15 @@
+/**
+ * function gets a non-negative integer number
+ * the function returns true if the number is possible
+ * result that is power of 2 by some number
+ * 
+ * @param {number} num1 a non-negative number
+ * @returns {boolean} result that returns false if the number is possibly
+ *  result of 2 powered by some number
+ */
+function isPowerOf2(num1){
+  const num = 
+
+ }
+
+ module.exports = isPowerOf2;
