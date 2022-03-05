763d29ab908167a8898a06b76eb93d7b3d6fc530
afekTheMiniLearner <afeksa2000@gmail.com>
2022-03-06 00:12:14 +0200

    hey

---
+++ arrays/__tests__/isPowerOf2.test.js
@@ -0,0 +1,11 @@
+const isPowerOf2 = require('../isPowerOf2');
+
+test('check if num1 can be expressed as 2 powered by some number', () =>{
+
+  expect(isPowerOf2(1)).toBe(true);
+})
+
+test('check if num1 cant be expressed as 2 powered by some number', () =>{
+
+  expect(isPowerOf2(98)).toBe(false);
+})
