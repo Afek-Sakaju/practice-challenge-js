6717035fcb2aaf5213d4a364b6c29aafb34e1e76
afekTheMiniLearner <afeksa2000@gmail.com>
2022-03-10 14:49:20 +0200

    333

---
+++ recourse/__tests__/allPositive.test.js
@@ -0,0 +1,5 @@
+const allPositive = require('../allPositive');
+
+test.each('returns 1 if all the array numbers are positive else returns 0', () => {
+  expect(allPositive(arr1)).to
+})
---
+++ recourse/allPositive.js
@@ -0,0 +1,11 @@
+/**
+ * the function gets array of numbers and if all 
+ * the numbers are positive returns 1, else returns 0
+ * 
+ * @param {Array} arr1 
+ * @return {Number} returns 1 if all the array numbers
+ * are positive, else returns 0
+ */
+function allPositive(arr1){
+  
+}
