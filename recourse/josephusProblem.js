bfc079e7d0f45d82810d5710b91d26756c7be6df
afekTheMiniLearner <afeksa2000@gmail.com>
2022-03-12 14:30:26 +0200

    just themplate to the problem

---
+++ recourse/josephusProblem.js
@@ -0,0 +1,8 @@
+/*
+you get number of total persons totalPersons
+and killNumber which indicated that this number -1 persons are skipped and the person
+on the killNumber is killed in circle in a fixed direction.
+The task is to choose the safe place in the circle,
+so when you perform the operations starting from 1st place in 
+the circle, you are the last one remaining and survive.
+*/
