a9538d9a47277afa9afa1306b5dc350d804a611c
hdrielb <hdrielb@gmail.com>
2022-06-10 01:31:45 +0300

    update LCS

--- test.js
+++ test.js
@@ -1,48 +1,82 @@
-function LCS(X, Y){
-    const M = X.length;
-    const N = Y.length;
-    const c = new Array(N).fill(new Array(M).fill(0));
-    const b = new Array(N).fill(new Array(M).fill(''));
-    debugger
-    for(let i = 1; i < N; i++){
-        for(let j = 0; j < M; j++){
-            if(X[j] === Y[i]){
-                c[i][j] = (c[i-1][j-1])+1;
+// 𝑋=GAGTTCG
+// 𝑌=AATACTG
+// 𝑋𝑌=𝐴𝑇𝐶𝐺
+function LCS(x,y){
+    const X = x.length + 1;
+    const Y = y.length + 1;
+    const c = buildMatrix(X, Y, 0);
+    const b = buildMatrix(X, Y, '.');
+    for(let i = 1; i < X; i++){
+        for(let j = 1; j < Y; j++){
+            const [xi, yj] = [x[i-1], y[j-1]]
+            if(xi === yj){
+                c[i][j] = c[i-1][j-1] + 1;
                 b[i][j] = '↖';
             }
             //          '↑'   >=     '←'
             else if(c[i-1][j] >= c[i][j-1]){
-                c[i][j] = (c[i-1][j]);
+                c[i][j] = c[i-1][j];
                 b[i][j] = '↑';
             }
             //          '↑'   <      '←'
             else {
-                c[i][j] = (c[i][j-1]);
+                c[i][j] = c[i][j-1];
                 b[i][j] = '←';
             }
         }
+        console.log(`iteration ${i}`)
+        printMatrix(x, y, c, b);
     }
 
     return b;
 }
 
-function printLCS(b, X, Y, i = X.length, j = Y.length) {
-    if(i === 0 || j === 0){
-        return;
+function buildMatrix(R, C, F) {
+    const m = new Array(R).fill([])
+    for(let i = 0; i < R; i++){
+        m[i] = new Array(C).fill(F);
     }
-    if(b[i][j] === '↖'){
-        printLCS(b, X.substring(0, i-1), Y.substring(0, j-1), i-1, j-1);
-        console.log(X[i]);
-    }
-    else if(b[i][j] === '←'){
-        printLCS(b, X.substring(0, i), Y.substring(0, j-1), i, j-1);
-    }
-    else{
-        printLCS(b, X.substring(0, i-1), Y.substring(0, j), i-1, j);
-    }
+    return m;
 }
 
-const X = 'DBACA';
-const Y = 'ADACB';
-const b = LCS(X,Y);
-printLCS(b, X, Y)
+function printMatrix(x, y, ...ms) {
+    let r = 0;
+    let done = false
+    let str = '* * ' + ms.map(i => y.split('').join(' ')).join('   * * ') + '\n';
+    while (!done){
+        str += ms.map((m, i) => (x[r-1] ?? '*')+ ' ' + m[r]?.join(' ') ?? '').join('\t') + '\n';
+        r++;
+        if(ms.every(m => m[r] === undefined)){
+            done = true;
+        }
+    }
+    console.log(str);
+}
+
+function printLCS(b, X, Y, i = X.length, j = Y.length) {
+    let result = '';
+    function print(b, X, Y, i, j) {
+        if(i === 0 || j === 0) return;
+
+        console.log(`Go b[${i}][${j}] = ${b[i][j]}`);
+
+        if(b[i][j] === '↖'){
+            print(b, X.substring(0, i-1), Y.substring(0, j-1), i-1, j-1);
+            result += Y[j-1];
+        }
+        else if(b[i][j] === '←'){
+            print(b, X.substring(0, i), Y.substring(0, j-1), i, j-1);
+        }
+        else{
+            print(b, X.substring(0, i-1), Y.substring(0, j), i-1, j);
+        }
+    }
+    print(b, X, Y, i, j)
+    console.log();
+    console.log('LSC is:', result);
+    return result;
+}
+
+const X = 'GAGTTCG';
+const Y = 'AATACTG';
+printLCS(LCS(X,Y), X, Y)
