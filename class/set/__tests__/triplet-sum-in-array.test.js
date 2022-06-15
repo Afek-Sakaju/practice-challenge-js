baf9f0de7a32a97b2939528af84a0db5f1ddc6bb
afekTheMiniLearner <afeksa2000@gmail.com>
2022-06-15 22:48:28 +0300

    .

---
+++ class/set/__tests__/triplet-sum-in-array.test.js
@@ -0,0 +1,16 @@
+import { find3Numbers } from "../triplet-sum-in-array";
+
+describe("triplet-sum-in-array tests", () => {
+  test.each([
+    [[1, 4, 45, 6, 10, 8], 13],
+    [[1, 2, 4, 3, 6], 10],
+  ])("function accept array: %s, and x: %s than return true", (arr, x) => {
+    expect(find3Numbers(arr, x)).toBeTruthy();
+  });
+  test.each([
+    [[1, 2, 3, 4, 5], 100],
+    [[1, 4, 3, 6], 50],
+  ])("function accept array: %s, and x: %s than return false", (arr, x) => {
+    expect(find3Numbers(arr, x)).toBeFalsy();
+  });
+});
---
+++ class/set/triplet-sum-in-array.js
@@ -0,0 +1,7 @@
+/* Given an array arr of size n and an integer X. 
+Find if there's a triplet in the array 
+which sums up to the given integer X. 
+Expected Time Complexity: O(n2)
+Expected Auxiliary Space: O(1) */
+
+export function find3Numbers(arr, x) {}
