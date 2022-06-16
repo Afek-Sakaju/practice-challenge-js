35d3e8dc2486498d1b3d7a5e0d7d8fe36a84f73a
afekTheMiniLearner <afeksa2000@gmail.com>
2022-06-16 16:41:17 +0300

    .

--- class/set/triplet-sum-in-array.js
+++ class/set/triplet-sum-in-array.js
@@ -4,4 +4,26 @@ which sums up to the given integer X.
 Expected Time Complexity: O(n2)
 Expected Auxiliary Space: O(1) */
 
-export function find3Numbers(arr, x) {}
+module.exports.find3Numbers = function (arr, x) {
+    if (arr.length < 3) return false
+    arr.sort((a, b) => {
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
+    })
+
+    let limit = arr.length
+    for (let i = 0; i < limit - 2; i++) {
+        for (let j = i + 1; j < limit - 1; j++) {
+            for (let k = j + 1; k < limit; k++) {
+                let sum = arr[i] + arr[j] + arr[k]
+                if (sum === x) return true
+
+                if (sum > x) {
+                    limit = k
+                }
+            }
+        }
+    }
+    return false
+}
