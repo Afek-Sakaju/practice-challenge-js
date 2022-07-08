27e1bebf5cc42e6fd2f0f273c6ae314e650116c3
afekTheMiniLearner <afeksa2000@gmail.com>
2022-07-08 23:42:03 +0300

    deleted all the files that i transfered to the course project which they related more to

--- chapter-practice-p1/only-digits.js
+++
@@ -1,7 +0,0 @@
-// Q3: "How do you check if a string contains only digits?"
-
-module.exports.onlyDigits = function (str) {
-    return typeof str === 'string'
-        ? !isNaN(+str) && typeof +str === 'number'
-        : false;
-};
