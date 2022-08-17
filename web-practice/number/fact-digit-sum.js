3a0c21d5e949e6096d1d71344710e0d16946a673
afekTheMiniLearner <afeksa2000@gmail.com>
2022-08-17 13:12:40 +0300

    fact digit sum done

--- web-practice/number/fact-digit-sum.js
+++ web-practice/number/fact-digit-sum.js
@@ -6,25 +6,38 @@ You have to return a list of digits which represent the number X.
 Expected Time Complexity: O(X) where X ≤ 106
 Expected Space Complexity: O(X) */
 
+class Stack {
+    items = [];
+    push = (element) => this.items.push(element);
+    pop = () => this.items.pop();
+    isempty = () => this.items.length === 0;
+    empty = () => (this.items.length = 0);
+    size = () => this.items.length;
+}
+
+function factorial(n) {
+    if (n === 1) return n;
+
+    return n * factorial(n - 1);
+}
+
 module.exports.factDigit = function (num) {
-    const strOfNums = JSON.stringify(num);
+    const stack1 = new Stack();
+    stack1.items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let res = 0;
+    let arr = [];
 
-    function numsStrReader(str, i = 0) {
-        if (i === str.length) return;
+    while (true) {
+        if (stack1.isempty()) return;
 
-        sum += calcRec(+str.charAt(i));
+        let temp = stack1.pop();
+        let fTemp = factorial(temp);
 
-        numsStrReader(str, i + 1);
-    }
-
-    let x = 1;
-    let res = factorial(x);
-
-    while (num > res) {
-        if (num === res) return x;
-        else {
-            x++;
+        if (res + fTemp <= num) {
+            res += fTemp;
+            arr.unshift(temp);
+            if (res === num) return +arr.join('');
+            else stack1.items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         }
-        res = factorial(x);
     }
 };
