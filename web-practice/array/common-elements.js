a1e3d5a4e1f230cd052be52eeff640275936ec16
afekTheMiniLearner <afeksa2000@gmail.com>
2022-08-19 22:16:04 +0300

    .

---
+++ web-practice/array/common-elements.js
@@ -0,0 +1,4 @@
+/* Given three arrays sorted in increasing order. 
+Find the elements that are common in all three arrays.
+Note: can you take care of the duplicates 
+without using any additional Data Structure? */
