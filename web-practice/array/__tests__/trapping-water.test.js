ada1272daad0c4933a9ddff6cc88fd2f6b174b4b
afekTheMiniLearner <afeksa2000@gmail.com>
2022-08-22 21:44:23 +0300

    done

---
+++ web-practice/array/__tests__/trapping-water.test.js
@@ -0,0 +1,11 @@
+const { trappingWater } = require('../trapping-water');
+
+describe('trapping-water tests', () => {
+    test.each([
+        [[7, 4, 0, 9], 10],
+        [[6, 9, 9], 0],
+        [[3, 0, 0, 2, 0, 4], 10],
+    ])('function accept arr: %s than return result: %s', (arr, res) => {
+        expect(trappingWater(arr)).toBe(res);
+    });
+});
