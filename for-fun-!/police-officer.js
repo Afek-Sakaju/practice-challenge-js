d076975404a202f907731a364f809fc5d6ae64c0
afekTheMiniLearner <afeksa2000@gmail.com>
2022-09-19 18:01:26 +0300

    .

--- for-fun-!/police-officer.js
+++ for-fun-!/police-officer.js
@@ -6,6 +6,7 @@ class PoliceOfficer {
         this._crush = crush ?? 'Anna the new crew manager';
         this._gunAllowed = age > 21 ? true : false;
         this._birthday = birthday;
+        this._skill = age > 21 ? age - 21 : 0;
     }
 
     get name() {
@@ -32,6 +33,10 @@ class PoliceOfficer {
         return this._birthday.toDateString();
     }
 
+    get skill() {
+        return this._skill;
+    }
+
     set setName(name) {
         this._name = name;
     }
@@ -52,27 +57,48 @@ class PoliceOfficer {
         this._birthday = newDate;
     }
 
+    updateSkill() {
+        this._skill = this.age > 21 ? this._age - 21 : 0;
+    }
+
     allowGun() {
         if (this._age <= 21) return false;
 
-        this._gunAllowed = bool;
+        this._gunAllowed = true;
         return true;
+    }
+
+    fightAlien(aliensSkill, aliensCount) {
+        if (this._gunAllowed === false) return "can't fight without a gun!";
+
+        const fightLog = {
+            skillDifference: 0,
+            winner: '',
+            time: new Date().toDateString(),
+        };
+
+        if (this._skill > aliensSkill) {
+            fightLog.skillDifference = this._skill - aliensSkill * aliensCount;
+            fightLog.winner = 'police-officer';
+        } else if (this._skill < alienSkill) {
+            fightLog.skillDifference = aliensSkill * aliensCount - this._skill;
+            fightLog.winner = 'aliens';
+        } else {
+            fightLog.skillDifference = 0;
+            fightLog.winner = 'its a tie, all of them lost consciousness';
+        }
+
+        return fightLog;
     }
 
     static whatsForBreakfast() {
         return 'sugar donuts !!';
     }
-
-    static whatsForLaunch() {
-        return 'choclate donuts !!';
-    }
-
-    static whatsForDinner() {
-        return 'maple donuts !!!';
-    }
 }
 
-const day = new Date('2000', '09', '29');
-const sarah = new PoliceOfficer('sarah', 21, 'nuriel', day);
-console.log(sarah.age);
-console.log(sarah.birthday);
+const bDay = new Date('2000', '09', '29');
+const sarah = new PoliceOfficer('sarah', 31, 'nuriel', bDay);
+
+console.log(sarah.skill);
+sarah.updateSkill();
+console.log(sarah.fightAlien(2, 3));
