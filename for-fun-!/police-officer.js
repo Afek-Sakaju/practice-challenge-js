3a8cd8947d5ef4f496987c5218c2931d0fe712b5
afekTheMiniLearner <afeksa2000@gmail.com>
2022-10-15 15:47:00 +0300

    .

--- for-fun-!/police-officer.js
+++ for-fun-!/police-officer.js
@@ -1,35 +1,57 @@
-class PoliceOfficer {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
+if (false) {
+    class PoliceOfficer {
+        constructor(name, age) {
+            this._name = name;
+            this._age = age;
+        }
+
+        get name() {
+            return this._name;
+        }
+
+        get age() {
+            return this._age;
+        }
+
+        set setName(name) {
+            this._name = name;
+        }
+
+        set setAge(num) {
+            this._age = num;
+        }
+
+        printName() {
+            console.log(`my name is ${this._name}`);
+        }
     }
 
-    get name() {
-        return this._name;
-    }
+    const officer1sarah = new PoliceOfficer('sarah', 20);
+    const officer2david = new PoliceOfficer('david', 30);
+    const officer3yaakov = new PoliceOfficer('yaakov', 40);
 
-    get age() {
-        return this._age;
-    }
+    officer1sarah.printName();
 
-    set setName(name) {
-        this._name = name;
-    }
+    const boundFunction = officer1sarah.printName.bind(officer2david);
 
-    set setAge(num) {
-        this._age = num;
-    }
+    boundFunction();
 
-    printName() {
-        console.log(`my name is ${this._name}`);
-    }
+    const arr1 = [1, 2, 3, 4];
+
+    console.log(arr1);
+
+    const arr2 = arr1.map((a) => {
+        return a * 100;
+    });
+
+    console.log(arr2);
 }
 
-const officer1sarah = new PoliceOfficer('sarah', 20);
-const officer2david = new PoliceOfficer('david', 30);
-const officer3yaakov = new PoliceOfficer('yaakov', 40);
+const stock = {
+    name: 'apple',
+    price: 55,
+    symbole: 'apl',
+};
 
-officer1sarah.printName();
-const boundFunction = officer1sarah.printName.bind(officer2david);
-
-boundFunction();
+const newStock = { ...stock};
+console.log(JSON.stringify(newStock));
