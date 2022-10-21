a40e74a7f5b6e7ae638916dfaf94e4399df7113a
afekTheMiniLearner <afeksa2000@gmail.com>
2022-10-21 20:31:30 +0300

    distinctString

---
+++ web-practice/string/distinct-chars-only.js
@@ -0,0 +1,21 @@
+/* Q: Write a function that accepts string and return the string
+without repetitive chars (only distinct) */
+
+function distinctString(str) {
+    const obj = {};
+    let res = '';
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charAt(i);
+
+        if (!obj.hasOwnProperty(char)) {
+            obj[char] = true;
+            res += char;
+        }
+    }
+
+    return res;
+}
+
+const result = distinctString('AABBBCCCDDDNNNNNNN')
+console.log(result);
