fec34089ee01dc3474d99381b1ac45d0225e63de
afekTheMiniLearner <afeksa2000@gmail.com>
2022-11-28 23:01:27 +0200

    fixed

--- refactore me/identical-check.js
+++ refactore me/identical-check.js
@@ -4,47 +4,27 @@ Two binary trees are identical
 if they have identical structure 
 and their contents are also the same. */
 
-module.exports.identicalTreeCheck = function (firstHead, SecondHead) {
+module.exports.identicalTreeCheck = function (head1, head2) {
     function sameStructureRec(node1, node2) {
-        if (node1.left && node2.left) {
-            if (sameStructureRec(node1.left, node2.left) === false)
-                return false;
-        } else if ((node1.left && !node2.left) || (!node1.left && node2.left)) {
-            return false;
-        }
-        if (node1.right && node2.right) {
-            if (sameStructureRec(node1.right, node2.right) === false)
-                return false;
-        } else if (
-            (node1.right && !node2.right) ||
-            (!node1.right && node2.right)
-        ) {
-            return false;
-        }
-        return true;
+        if (!node1 && !node2) return;
+        else if ((!node1 && node2) || (node1 && !node2)) return false;
+
+        return (
+            (sameStructureRec(node1.left, node2.left) &&
+                sameStructureRec(node1.right, node2.right)) !== false
+        );
     }
+
     function sameData(node1, node2) {
-        if (node1) {
-            if (node1.data !== node2.data) return false;
-            if (node1.left && sameData(node1.left, node2.left) === false) {
-                return false;
-            } else if (
-                node1.right &&
-                sameData(node1.right, node2.right) === false
-            ) {
-                return false;
-            }
-        }
+        if (!node1 || !node2) return true;
 
-        return true;
-    }
-    if ((firstHead && !SecondHead) || (!firstHead && SecondHead)) {
-        return false;
+        if (node1.data !== node2.data) return false;
+
+        return (
+            sameData(node1.left, node2.left) &&
+            sameData(node1.right, node2.right)
+        );
     }
 
-    return sameStructureRec(firstHead, SecondHead)
-        ? sameData(firstHead, SecondHead)
-        : false;
+    return sameStructureRec(head1, head2) && sameData(head1, head2);
 };
-
-//todo: refactore function
