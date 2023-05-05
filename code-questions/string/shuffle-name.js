234d0bc6eec932e60cc89a329d4027d9a20d9e02
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-05 19:25:25 +0300

    .

--- code-questions/string/shuffle-name.js
+++ code-questions/string/shuffle-name.js
@@ -1,6 +1,6 @@
 /* Write a function that accepts a string
-(of a person's first and last name)
-than returns string with the first and last name swapped. */
+of a person's first and last name and returns 
+string with the first and last name swapped. */
 
 module.exports.shuffle = function (fullName) {
     const [first, last] = fullName?.split(' ');
