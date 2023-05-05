2aa6d704db92050e0cc7f176d7b193b9750cb893
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-05 22:18:16 +0300

    zigzagSort finished and its tests

---
+++ code-questions/array/__tests__/zigzag-sort.test.js
@@ -0,0 +1,33 @@
+const { zigzagSort } = require('../zigzag-sort');
+
+describe('zigzag-sort tests', () => {
+    test.each([
+        [
+            [4, 3, 7, 8, 6, 2, 1],
+            [3, 7, 4, 8, 2, 6, 1],
+        ],
+        [
+            [1, 4, 3, 2],
+            [1, 4, 2, 3],
+        ],
+        [
+            [2, 4, 3, 5],
+            [2, 4, 3, 5],
+        ],
+        [
+            [2, 4],
+            [2, 4],
+        ],
+        [
+            [4, 2],
+            [2, 4],
+        ],
+        [[2], [2]],
+        [[], []],
+    ])(
+        'function accept array: %s, then returns zigzag sorted arr: %s',
+        (arr, res) => {
+            expect(zigzagSort(arr)).toEqual(res);
+        }
+    );
+});
