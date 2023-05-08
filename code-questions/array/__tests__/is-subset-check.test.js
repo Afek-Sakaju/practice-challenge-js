a5291494e825a241f5975e7994c0aae3418df627
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-09 00:40:33 +0300

    isSubset refactre

--- code-questions/array/__tests__/is-subset-check.test.js
+++ code-questions/array/__tests__/is-subset-check.test.js
@@ -1,14 +1,32 @@
 const { isSubset } = require('../is-subset-check');
 
 describe('is-subset-check tests', () => {
-    test.each([
-        [[11, 1, 13, 21, 3, 7], [11, 3, 7, 1], 'Yes'],
-        [[1, 2, 3, 4, 5, 6], [1, 2, 4], 'Yes'],
-        [[10, 5, 2, 23, 19], [19, 5, 3], 'No'],
-    ])(
-        'function accepts two arrays: %s and %s, then return: %s',
-        (arr1, arr2, res) => {
-            expect(isSubset(arr1, arr2)).toBe(res);
-        }
-    );
+    describe('good cases', () => {
+        test.each([
+            [[11, 1, 13, 21, 3, 7], [11, 3, 7, 1], 'Yes'],
+            [[1, 2, 3, 4, 5, 6], [1, 2, 4], 'Yes'],
+            [['a', 'b', 'c'], ['c'], 'Yes'],
+            [['hello', 'w', 'o', 'r', 'l', 'd'], ['z', 'w', 'r'], 'No'],
+            [[1, 23, 13, 1], [4], 'No'],
+        ])(
+            'function accepts two arrays: %s and %s, then return: %s',
+            (arr1, arr2, res) => {
+                expect(isSubset(arr1, arr2)).toBe(res);
+            }
+        );
+    });
+
+    describe('bad cases', () => {
+        test.each([
+            [[], [19, 5, 3], 'No'],
+            [[19, 5, 3], [], 'Yes'],
+            [[null, undefined], [null], 'Yes'],
+            [[], [], 'Yes'],
+        ])(
+            'function accepts both arrays: %s and %s, then return: %s',
+            (arr1, arr2, res) => {
+                expect(isSubset(arr1, arr2)).toBe(res);
+            }
+        );
+    });
 });
--- code-questions/array/is-subset-check.js
+++ code-questions/array/is-subset-check.js
@@ -1,63 +1,59 @@
 /* Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m. 
 Task is to check whether a2[] is a subset of a1[] or not. 
 Both the arrays can be sorted or unsorted. 
- 
+
 Example 1:
 
 Input:
 a1[] = {11, 1, 13, 21, 3, 7}
 a2[] = {11, 3, 7, 1}
-Output:
-Yes
-Explanation:
-a2[] is a subset of a1[]
+
+Output: Yes
+
+Explanation: a2[] is a subset of a1[]
 
 Example 2:
 
 Input:
 a1[] = {1, 2, 3, 4, 5, 6}
 a2[] = {1, 2, 4}
-Output:
-Yes
-Explanation:
-a2[] is a subset of a1[]
+
+Output: Yes
+Explanation: a2[] is a subset of a1[]
 
 Example 3:
 
 Input:
 a1[] = {10, 5, 2, 23, 19}
 a2[] = {19, 5, 3}
-Output:
-No
-Explanation:
-a2[] is not a subset of a1[]
- 
+
+Output: No
+Explanation: a2[] is not a subset of a1[]
 
 Your Task:  
 You don't need to read input or print anything. 
-Your task is to complete the function isSubset() 
-which takes the array a1[], a2[], 
-its size n and m as inputs 
-and return "Yes" if arr2 is subset of arr1 
-else return "No" if arr2 is not subset of arr1.
+Your task is to complete the function isSubset() which takes 
+the array a1[], a2[], its size n and m as inputs 
+and return "Yes" if arr2 is subset of arr1 else return "No".
 
 Expected Time Complexity: O(n)
 Expected Auxiliary Space: O(n) */
 
 module.exports.isSubset = function (arr1, arr2) {
-    const obj = {};
-    let total = 0;
+    const elementsCounter = {};
+    let commonsCount = 0;
 
-    for (const num of arr2) {
-        if (!obj[num]) obj[num] = 1;
-        else obj[num]++;
-    }
+    arr2.forEach((element) => {
+        if (elementsCounter[element]) elementsCounter[element]++;
+        else elementsCounter[element] = 1;
+    });
 
-    for (let i = 0; i < arr1.length; i++) {
-        if (obj[arr1[i]] > 0) {
-            obj[arr1[i]]--;
-            total++;
+    arr1.forEach((element) => {
+        if (elementsCounter[element] > 0) {
+            elementsCounter[element]--;
+            commonsCount++;
         }
-    }
-    return total === arr2.length ? 'Yes' : 'No';
+    });
+    
+    return commonsCount === arr2.length ? 'Yes' : 'No';
 };
