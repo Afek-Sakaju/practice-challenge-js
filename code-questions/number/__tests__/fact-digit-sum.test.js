9969937c3db5455347da3dd10b7b548035ae96e5
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-05-10 16:55:29 +0300

    finished to refactor some number function files

--- code-questions/number/__tests__/fact-digit-sum.test.js
+++ code-questions/number/__tests__/fact-digit-sum.test.js
@@ -2,9 +2,8 @@ const { factDigit } = require('../fact-digit-sum');
 
 describe('fact-digit-sum tests', () => {
     test.each([
-        [5040, 7],
-        [40321, 18],
         [40440, 58],
+        [5040, 7],
         [0, 0],
         [3, 12],
         [21, 12333],
--- code-questions/number/fact-digit-sum.js
+++ code-questions/number/fact-digit-sum.js
@@ -1,13 +1,13 @@
-/* A(X) for positive integer X is 
-the sum of factorials of its digits. 
+/* A(X) for positive integer X is the sum of factorials of its digits. 
 For example, A(154) = 1! + 5! + 4!= 145.
-Given a number N, find the minimum number X such that A(X) = N. 
+Given a number N, find the minimum number X such that A(X) = N.
 You have to return a list of digits which represent the number X. 
+
 Expected Time Complexity: O(X) where X ≤ 106
 Expected Space Complexity: O(X) */
 
-module.exports.factDigit = function (num) {
-    const obj = {
+module.exports.factDigitSum = function (num) {
+    const numbersFactorials = {
         1: 1,
         2: 2,
         3: 6,
@@ -19,16 +19,21 @@ module.exports.factDigit = function (num) {
         9: 362880,
     };
 
-    let res = 0;
-    let arr = [];
+    let sum = 0;
+    let digits = [];
 
-    for (let i = 9; i > 0; i--) {
-        if (res + obj[i] <= num) {
-            res += obj[i];
-            arr.unshift(i);
-            i++;
-            if (res === num) return +arr.join('');
-        }
-    }
-    return 0;
+    Object.entries(numbersFactorials)
+        // Reverse looping on the object entries from the biggest to the smallest
+        .reverse()
+        .forEach(([digit, factorial]) => {
+            if (sum === num) return;
+
+            const shouldAddDigit = sum + factorial <= num;
+            if (shouldAddDigit) {
+                sum += factorial;
+                digits.unshift(digit);
+            }
+        });
+
+    return +digits.join('');
 };
